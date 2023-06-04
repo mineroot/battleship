30e3120ccaa0752b184e40b936c62661b06a7f71
Pavlo Dosenko <daggett.ua@gmail.com>
2023-06-04 17:03:06 +0300

    refactoring, prevent dragging multiple ships

--- pkg/gui/compo/compo_field.go
+++ pkg/gui/compo/compo_field.go
@@ -9,6 +9,7 @@ import (
 
 type Field struct {
 	base
+	ships []*Ship
 }
 
 func NewField() *Field {
@@ -19,8 +20,9 @@ func NewField() *Field {
 			Max: size,
 		})),
 	}
-	f.Pos = pixel.V(300, 300)
+	f.Pos = pixel.V(300, 400)
 	f.Size = size
+	f.initShipsSetupScene()
 	imd := imdraw.New(nil)
 	imd.Color = colornames.Blue
 	imd.EndShape = imdraw.RoundEndShape
@@ -30,6 +32,33 @@ func NewField() *Field {
 	return f
 }
 
-func (f *Field) Update(*pixelgl.Window, float64) {
-	// nop
+func (f *Field) Update(win *pixelgl.Window, dt float64) {
+	for _, ship := range f.ships {
+		ship.Update(win, dt)
+		if ship.isMouseDown {
+			break // prevent dragging multiple ships
+		}
+	}
+}
+
+func (f *Field) Draw(target pixel.ComposeTarget, dt float64) {
+	f.base.Draw(target, dt)
+	for _, ship := range f.ships {
+		ship.Draw(target, dt)
+	}
+}
+
+func (f *Field) initShipsSetupScene() {
+	f.ships = []*Ship{
+		NewShip(1, pixel.V(775, 225)),
+		NewShip(1, pixel.V(875, 225)),
+		NewShip(1, pixel.V(975, 225)),
+		NewShip(1, pixel.V(1075, 225)),
+		NewShip(2, pixel.V(800, 325)),
+		NewShip(2, pixel.V(950, 325)),
+		NewShip(2, pixel.V(1100, 325)),
+		NewShip(3, pixel.V(825, 425)),
+		NewShip(3, pixel.V(1025, 425)),
+		NewShip(4, pixel.V(850, 525)),
+	}
 }
--- pkg/gui/scene/scene_setup.go
+++ pkg/gui/scene/scene_setup.go
@@ -13,21 +13,11 @@ type SetupScene struct {
 }
 
 func NewSetupScene(bounds pixel.Rect, player game.Player) *SetupScene {
-	//newGame := game.NewGame()
-	//_ = newGame
+	newGame := game.NewGame()
+	_ = newGame
 	components := []compo.Compo{
 		compo.NewBg(bounds),
 		compo.NewField(),
-		compo.NewShip(1, pixel.V(775, 125)),
-		compo.NewShip(1, pixel.V(875, 125)),
-		compo.NewShip(1, pixel.V(975, 125)),
-		compo.NewShip(1, pixel.V(1075, 125)),
-		compo.NewShip(2, pixel.V(800, 225)),
-		compo.NewShip(2, pixel.V(950, 225)),
-		compo.NewShip(2, pixel.V(1100, 225)),
-		compo.NewShip(3, pixel.V(825, 325)),
-		compo.NewShip(3, pixel.V(1025, 325)),
-		compo.NewShip(4, pixel.V(850, 425)),
 	}
 	return &SetupScene{
 		components: components,
